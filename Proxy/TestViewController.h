70287de82800a4e466aa96bbcb4bc611d9918d3b
Flying <iamflying@vip.qq.com>
2020-09-04 17:35:57 +0800

    111

--- Proxy/TestViewController.h
+++ Proxy/TestViewController.h
@@ -13,7 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface TestViewController : UIViewController
 
-@property (nonatomic, weak) id delegate;
+@property (nonatomic, weak) id<RSTestProtocol> delegate;
 
 @end
 
